90082e826d150cb9936c59d45e7cc5ef6ce64181
Илья <qq.ferveks@gmail.com>
2021-08-20 12:57:31 +0300

    2. CandidateSerlvet. Создание кандидата. [#2513 #191472]

--- src/main/java/ru/job4j/dream/Store/Store.java
+++ src/main/java/ru/job4j/dream/Store/Store.java
@@ -12,6 +12,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Store {
     private static final Store INST = new Store();
     private static AtomicInteger POST_ID = new AtomicInteger(4);
+    private static AtomicInteger CANDIDATE_ID = new AtomicInteger(4);
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
     private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
 
@@ -44,7 +45,7 @@ public class Store {
         posts.put(post.getId(), post);
     }
     public void saveCandidate(Candidate candidate) {
-        candidate.setId(POST_ID.incrementAndGet());
+        candidate.setId(CANDIDATE_ID.incrementAndGet());
         candidates.put(candidate.getId(), candidate);
     }
 }
