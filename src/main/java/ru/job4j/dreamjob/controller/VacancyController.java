c499f0548532aa2faf2a5d2879b93d0782f0d62c
ferveks3509 <qq.ferveks@gmail.com>
2023-05-13 09:07:22 +0300

    Слоеная архитектура

--- src/main/java/ru/job4j/dreamjob/controller/VacancyController.java
+++ src/main/java/ru/job4j/dreamjob/controller/VacancyController.java
@@ -4,8 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.job4j.dreamjob.model.Vacancy;
-import ru.job4j.dreamjob.repository.VacancyRepository;
-import ru.job4j.dreamjob.repository.memory.MemoryVacancyRepository;
+import ru.job4j.dreamjob.service.SimpleVacancyService;
+import ru.job4j.dreamjob.service.VacancyService;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
@@ -14,11 +14,11 @@ import java.time.LocalDateTime;
 @RequestMapping("/vacancies")
 public class VacancyController {
 
-    private final VacancyRepository vacancyRepository = MemoryVacancyRepository.getInstance();
+    private final VacancyService vacancyService = SimpleVacancyService.getInstance();
 
     @GetMapping
     public String getAll(Model model) {
-        model.addAttribute("vacancies", vacancyRepository.findAll());
+        model.addAttribute("vacancies", vacancyService.findAll());
         return "vacancies/list";
     }
 
@@ -31,12 +31,12 @@ public class VacancyController {
     public String create(HttpServletRequest request) {
         var title = request.getParameter("title");
         var description = request.getParameter("description");
-        vacancyRepository.save(new Vacancy(0, title, description, LocalDateTime.now()));
+        vacancyService.save(new Vacancy(0, title, description, LocalDateTime.now()));
         return "redirect:/vacancies";
     }
     @GetMapping("/{id}")
     public String getById(Model model, @PathVariable int id) {
-        var vacancyOptional = vacancyRepository.findById(id);
+        var vacancyOptional = vacancyService.findById(id);
         if (vacancyOptional.isEmpty()) {
             model.addAttribute("message", "Вакансия с указанным идентификатором не найдена");
             return "errors/404";
@@ -47,7 +47,7 @@ public class VacancyController {
 
     @PostMapping("/update")
     public String update(@ModelAttribute Vacancy vacancy, Model model) {
-        var isUpdated = vacancyRepository.update(vacancy);
+        var isUpdated = vacancyService.update(vacancy);
         if (!isUpdated) {
             model.addAttribute("message", "Вакансия с указанным идентификатором не найдена");
             return "errors/404";
@@ -57,7 +57,7 @@ public class VacancyController {
 
     @GetMapping("/delete/{id}")
     public String delete(Model model, @PathVariable int id) {
-        var isDeleted = vacancyRepository.deleteById(id);
+        var isDeleted = vacancyService.deleteById(id);
         if (!isDeleted) {
             model.addAttribute("message", "Вакансия с указанным идентификатором не найдена");
             return "errors/404";
---
+++ src/main/java/ru/job4j/dreamjob/service/SimpleVacancyService.java
@@ -0,0 +1,49 @@
+package ru.job4j.dreamjob.service;
+
+import org.springframework.stereotype.Service;
+import ru.job4j.dreamjob.model.Vacancy;
+import ru.job4j.dreamjob.repository.CandidateRepository;
+import ru.job4j.dreamjob.repository.VacancyRepository;
+import ru.job4j.dreamjob.repository.memory.MemoryVacancyRepository;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@Service
+public class SimpleVacancyService implements VacancyService {
+
+    private static final SimpleVacancyService INSTANCE = new SimpleVacancyService();
+
+    private final VacancyRepository vacancyRepository = MemoryVacancyRepository.getInstance();
+
+    private SimpleVacancyService() { }
+
+    public static SimpleVacancyService getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public Vacancy save(Vacancy vacancy) {
+        return vacancyRepository.save(vacancy);
+    }
+
+    @Override
+    public boolean deleteById(int id) {
+        return vacancyRepository.deleteById(id);
+    }
+
+    @Override
+    public boolean update(Vacancy vacancy) {
+        return vacancyRepository.update(vacancy);
+    }
+
+    @Override
+    public Optional<Vacancy> findById(int id) {
+        return vacancyRepository.findById(id);
+    }
+
+    @Override
+    public Collection<Vacancy> findAll() {
+        return vacancyRepository.findAll();
+    }
+}
