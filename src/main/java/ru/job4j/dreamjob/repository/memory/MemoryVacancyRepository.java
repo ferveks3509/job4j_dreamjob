51ed9a180db3708358db467c4997982e32f61e4e
ferveks3509 <qq.ferveks@gmail.com>
2023-05-14 17:58:14 +0300

    Формы. Загрузка файла на сервер для vacancy

--- src/main/java/ru/job4j/dreamjob/repository/memory/MemoryVacancyRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/memory/MemoryVacancyRepository.java
@@ -15,12 +15,12 @@ public class MemoryVacancyRepository implements VacancyRepository {
     private final Map<Integer, Vacancy> vacancies = new HashMap<>();
 
     private MemoryVacancyRepository() {
-        save(new Vacancy(0, "Intern Java Developer", "desc", LocalDateTime.now(), false, 1));
-        save(new Vacancy(0, "Junior Java Developer", "desc", LocalDateTime.now(), false, 1));
-        save(new Vacancy(0, "Junior+ Java Developer", "desc", LocalDateTime.now(), false, 1));
-        save(new Vacancy(0, "Middle Java Developer", "desc", LocalDateTime.now(), false, 1));
-        save(new Vacancy(0, "Middle+ Java Developer", "desc", LocalDateTime.now(), false, 1));
-        save(new Vacancy(0, "Senior Java Developer", "desc", LocalDateTime.now(), false, 1));
+        save(new Vacancy(0, "Intern Java Developer", "Стажер Java разработчик", LocalDateTime.now(), true, 1, 0));
+        save(new Vacancy(0, "Junior Java Developer", "Младший Java разработчик", LocalDateTime.now(), true, 1, 0));
+        save(new Vacancy(0, "Junior+ Java Developer", "Java разработчик", LocalDateTime.now(), true, 2, 0));
+        save(new Vacancy(0, "Middle Java Developer", "Старший Java разработчик", LocalDateTime.now(), true, 2, 0));
+        save(new Vacancy(0, "Middle+ Java Developer", "Ведущий Java разработчик", LocalDateTime.now(), true, 2, 0));
+        save(new Vacancy(0, "Senior Java Developer", "Главный Java разработчик", LocalDateTime.now(), true, 3, 0));
     }
 
     @Override
@@ -43,7 +43,8 @@ public class MemoryVacancyRepository implements VacancyRepository {
                 vacancy.getDescription(),
                 vacancy.getCreationDate(),
                 vacancy.getVisible(),
-                vacancy.getCityId())) != null;
+                vacancy.getCityId(),
+                vacancy.getFileId())) != null;
     }
 
     @Override
